4bd6418f057cdf58ac97ca0b23a7fb764b74cd28
Daniel Sochor <47047939+DanielSochor@users.noreply.github.com>
2019-04-10 20:56:58 -0500

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
 
+    //TODO: fix button border, time remaining so height doesn't change, colors
+
+    var questionsAndAnswers = questionObject;
+
+    console.log(questionsAndAnswers);
+
+    //shuffle questions and option
+
+    //button hover is same for start and each option
+
     var question1 = {
-        question:"Which is not an American Car Manufacturer",
-        answer1:"Ford",
-        answer2:"GM",
-        answer3:"Mazda",
-        answer4:"Dodge",
+        question: "Which is not an American Car Manufacturer",
+        answer1: "Ford",
+        answer2: "GM",
+        answer3: "Mazda",
+        answer4: "Dodge",
     }
 
     var question2 = {
@@ -32,32 +42,129 @@ $(document).ready(function () {
 
     var questions = [question1, question2, question3];
 
-    populateQuestion();
+    var availableQuestionsToGuess = 1;
+    var questionDisplayed = false;
+    var answer = "button1";
 
-    function populateQuestion(){
-        var selectedQuestion = questions[Math.floor(Math.random() * questions.length)];
-        $("#question").text(selectedQuestion.question);
-        $("#answerChoice1").text(selectedQuestion.answer1);
-        $("#answerChoice2").text(selectedQuestion.answer2);
-        $("#answerChoice3").text(selectedQuestion.answer3);
-        $("#answerChoice4").text(selectedQuestion.answer4);
+    gameOpeningScreen();
+
+    function gameOpeningScreen() {
+        $(".button").hide();
+        $("#button1").show().text("Start!");
     }
 
-    var timeLeft = 30;
-    var timerId = setInterval(countdown, 1000);
+    $(".button").on("click", function () {
+        buttonPressed = $(this).attr("id");
+        game();
+    });
 
-    function countdown() {
-        if (timeLeft == -1) {
-            clearTimeout(timerId);
-            doSomething();
+    function game() {
+        if (availableQuestionsToGuess > 0) {
+            if (questionDisplayed == false) {
+                populateQuestion();
+                //countdown();
+                timer();
+                questionDisplayed = true;
+                thereIsTimeLeft = true;
+            } else if (buttonPressed == answer) {
+                correctAnswer();
+            } else if (buttonPressed != answer) {
+                incorrectAnswer();
         } else {
-            $("#timer").text(timeLeft + ' seconds remaining');
-            timeLeft--;
+            gameOver();
+        }
+
+    }
+
+    function correctAnswer() {
+        console.log("that is the correct answer");
+        clearInterval(timerId);
+    }
+
+    function incorrectAnswer() {
+        console.log("that is not correct answer");
+    }
+
+    function outOfTime() {
+        console.log("you ran out of time");
+    }
+
+    function showCorrectAnswer() {
+        time = 5
+        timer()
+    }
+
+    function gameOver(){
+        console.log("you ran out of time");
+        //show score
+        //show replay button
+    }
+
+    function populateQuestion() {
+        console.log("pop");
+        $(".button").show();
+        var selectedQuestion = questions[Math.floor(Math.random() * questions.length)];
+        $("#question").text(selectedQuestion.question);
+        $("#button1").text(selectedQuestion.answer1);
+        $("#button2").text(selectedQuestion.answer2);
+        $("#button3").text(selectedQuestion.answer3);
+        $("#button4").text(selectedQuestion.answer4);
+    }
+
+
+
+
+    //reset timer value for answering questions and for showing correct answer
+    //var timeIsUp = false;
+    var time = 30;
+    //var timerId = setInterval(countdown, 1000);
+    function timer() {
+        var timerId = setInterval(countdown, 1000);
+        function countdown() {
+            if (time == -1) {
+                clearTimeout(timerId);
+                $("#timer").text("Time is up");
+                outOfTime();
+            } else {
+                $("#timer").text(time + ' seconds remaining');
+                time--;
+            }
+            //return (time);
         }
     }
 
-    function doSomething() {
-        $("#timer").text("Time is up");
+
+
+    // var countDown = 0;
+    // var answer = 1;
+
+    // function gamle() {
+    //     $(".button").show();
+    //     console.log("game");
+    //     if (availableQuestionsToGuess > 0) {
+    //         time = 30;
+    //         countdown(time);
+    //         populateQuestion();
+    //         if (time >= 0) {
+    //             if (answer = 1) {
+    //                 // you are correct
+    //                 console.log("you are correct");
+    //             } else {
+    //                 //ancser is false
+    //                 //timer is 5 seconds
+    //                 correctAnswer();
+    //                 //call game()
+    //             }
+    //         } else {
+    //             //run out of time
+    //             correctAnswer();
+    //         }
+    //     } else {
+    //         //show results
+    //         //show play again
+    //     }
+    // }
     }
+   
 
 });
---
+++ assets/javascript/questions.js
@@ -0,0 +1,60 @@
+// $(document).ready(function () {
+
+    var questionObject = {
+        question1: {
+            question: "We can hook onto the event of a user pressing and releasing a key by referring to ____.",
+            options: [
+                {
+                    A: "event.key"
+                },
+                {
+                    B: "document.onkeyup"
+                },
+                {
+                    C: "Answer C"
+                },
+                {
+                    D: "Answer D"
+                }],
+            answer: "B"
+        },
+        question2: {
+            question: "We can get the key a user has pressed by referencing ____.",
+            options: [
+                {
+                    A: "event.key"
+                },
+                {
+                    B: "document.onkeyup"
+                },
+                {
+                    C: "Answer C"
+                },
+                {
+                    D: "Answer D"
+                }],
+            answer: "A"
+        },
+        question3: {
+            question: "A function's ____ are only visble inside the function body.",
+            options: [
+                {
+                    A: "Answer A"
+                },
+                {
+                    B: "Answer B"
+                },
+                {
+                    C: "defintions"
+                },
+                {
+                    D: "Arguments"
+                }],
+            answer: "D"
+        }
+    }
+
+
+
+
+// });
